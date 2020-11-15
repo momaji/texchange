b2f91c76d8c8c410daef1a1db5250e55112dea3a
MohammadH98 <mohammad.1011@hotmail.com>
2020-11-15 17:14:33 -0500

    profile changes are now saved, removed unneeded states

--- src/profile.js
+++ src/profile.js
@@ -23,13 +23,6 @@ class Profile extends React.Component{
         super(props);
         this.state = {
             profileID: this.props.match.params.id,
-            firstName: "",
-            lastName: "",
-            gender: "",
-            email: "",
-            number: "",
-            location: "",
-            image: "",
             rating: 0,
             books: [],
             favourited: [],
@@ -80,13 +73,6 @@ class Profile extends React.Component{
         this.setState
         ({
             profileID: this.props.match.params.id,
-            firstName: profileData["firstName"],
-            lastName: profileData["lastName"],
-            gender: profileData["gender"],
-            email: profileData["email"],
-            number: profileData["phone"],
-            location: profileData["location"],
-            image: profileData["avatar"],
             rating: numStars,
             books: bookList,
             favourited: favouritedList
@@ -120,18 +106,14 @@ class Profile extends React.Component{
 
     editFields()
     {
-        this.setState
-        ({
-            editable: !this.state.editable
-        });
-
+        this.setState({editable: !this.state.editable});
         this.props.editProfile();
     }
 
-    render(){
-    let profile = this.props.appData.people.filter(person => person.id == this.props.match.params.id )[0]
-    console.log(profile)
-    let bookList2 = this.props.appData.textbooks.filter(book => book.personId == this.state.profileID )
+    render()
+    {
+        let profile = this.props.appData.people.filter(person => person.id == this.props.match.params.id )[0]
+        let bookList2 = this.props.appData.textbooks.filter(book => book.personId == this.state.profileID )
 
         return (
             <Container fluid="true">
@@ -140,13 +122,13 @@ class Profile extends React.Component{
                     <Col sm={1}>
                     </Col>
                     <Col>
-                        <h1 className="float-left">{this.state.firstName} {this.state.lastName}'s Profile
-                            {this.state.profileID === "0" && <Button className={!this.state.editable ? "" : "btn-secondary"} onClick={this.editFields.bind(this)} style={{marginLeft: "25px"}} size="lg">
+                        <h1 className="float-left">{profile.firstName} {profile.lastName}'s Profile
+                            {profile.id === 0 && <Button className={!this.state.editable ? "" : "btn-secondary"} onClick={this.editFields.bind(this)} style={{marginLeft: "25px"}} size="lg">
                             {!this.state.editable ? <FaPencilAlt></FaPencilAlt> : <FaRegSave></FaRegSave>}</Button>}
                         </h1>
                         <h2 className="float-right">Seller Rating:
-                            {this.state.rating !== 0 && this.state.profileID !== "0" && <ReactStars {...thirdExample} value={this.state.rating} onChange={this.changeRating} />}
-                            {this.state.rating !== 0 && this.state.profileID === "0" && <ReactStars {...thirdExample} value={this.state.rating} edit={false} />}
+                            {this.state.rating !== 0 && profile.id !== 0 && <ReactStars {...thirdExample} value={this.state.rating} onChange={this.changeRating} />}
+                            {this.state.rating !== 0 && profile.id === 0 && <ReactStars {...thirdExample} value={this.state.rating} edit={false} />}
                         </h2>
                     </Col>
                     <Col sm={1}>
@@ -156,33 +138,33 @@ class Profile extends React.Component{
                     <Col sm={1}>
                     </Col>
                     <Col>
-                        {this.state.editable ? <Form inline><Form.Label>Upload a different picture</Form.Label><Form.Control type="file" onChange={this.props.editPic}></Form.Control></Form>: <Image rounded thumbnail className="float-left" src={profile.avatar} alt={this.state.firstName} width="350" height="350"></Image>}
+                        {this.state.editable ? <Form inline><Form.Label>Upload a different picture</Form.Label><Form.Control type="file" onChange={this.props.editPic}></Form.Control></Form>: <Image rounded thumbnail className="float-left" src={profile.avatar} alt={profile.firstName} width="350" height="350"></Image>}
 
                         <div style={{paddingTop: "17px"}}>
                             <h4 style={{paddingLeft: "20px"}} className="float-left"><Form inline><Form.Label className="font-weight-bold">Full Name:&nbsp;</Form.Label>
                                 {this.state.editable ? <div><Form.Control defaultValue={this.props.firstName} type="input" value={this.props.firstName} onChange={this.props.editFirstName}></Form.Control>
                                 <Form.Control defaultValue={this.props.lastName} type="input" value={this.props.lastName} onChange={this.props.editLastName}></Form.Control></div>
-                                : this.state.firstName + " " + this.state.lastName}</Form>
+                                : profile.firstName + " " + profile.lastName}</Form>
                             </h4><br></br><br></br><br></br>
 
                             <h4 style={{paddingLeft: "20px"}} className="float-left"><Form inline><Form.Label className="font-weight-bold">Gender:&nbsp;</Form.Label>
                                 {this.state.editable ? <Form.Control defaultValue={this.props.gender} as="select" value={this.props.gender} onChange={this.props.editGender}>
-                                <option>Male</option><option>Female</option><option>Other</option></Form.Control> : this.state.gender}</Form>
+                                <option>Male</option><option>Female</option><option>Other</option></Form.Control> : profile.gender}</Form>
                             </h4><br></br><br></br><br></br>
 
                             <h4 style={{paddingLeft: "20px"}} className="float-left"><Form inline><Form.Label className="font-weight-bold">Email:&nbsp;</Form.Label>
                                 {this.state.editable ? <Form.Control defaultValue={this.props.email} type="input" value={this.props.email} onChange={this.props.editEmail}>
-                                </Form.Control> : this.state.email}</Form>
+                                </Form.Control> : profile.email}</Form>
                             </h4><br></br><br></br><br></br>
 
                             <h4 style={{paddingLeft: "20px"}} className="float-left"><Form inline><Form.Label className="font-weight-bold">Phone Number:&nbsp;</Form.Label>
-                                {this.state.editable ? <Form.Control defaultValue={this.props.number} type="input" value={this.props.number} onChange={this.props.editPhone}>
-                                </Form.Control> : this.state.number}</Form>
+                                {this.state.editable ? <Form.Control defaultValue={this.props.phone} type="input" value={this.props.phone} onChange={this.props.editPhone}>
+                                </Form.Control> : profile.phone}</Form>
                             </h4><br></br><br></br><br></br>
 
                             <h4 style={{paddingLeft: "20px"}} className="float-left"><Form inline><Form.Label className="font-weight-bold">Location:&nbsp;</Form.Label>
                                 {this.state.editable ? <Form.Control defaultValue={this.props.location} type="input" value={this.props.location} onChange={this.props.editLocation}>
-                                </Form.Control> : this.state.location}</Form>
+                                </Form.Control> : profile.location}</Form>
                             </h4><br></br><br></br><br></br>
                         </div>
                     </Col>
@@ -193,7 +175,7 @@ class Profile extends React.Component{
                     <Col sm={1}>
                     </Col>
                     <Col>
-                        <h1 className="float-left">{this.state.profileID === "0" ? "Your" : this.state.firstName + "'s"} Textbooks For Sale</h1>
+                        <h1 className="float-left">{profile.id === 0 ? "Your" : profile.firstName + "'s"} Textbooks For Sale</h1>
                     </Col>
                     <Col sm={1}>
                     </Col>
@@ -211,7 +193,7 @@ class Profile extends React.Component{
                                         <span className="font-weight-bold">{book.name}</span>
                                         <p>{book.author}</p>
                                         <p>${book.flexible? book.price+' - Flexible' : book.price}</p>
-                                        {this.state.profileID === "0" && <p><Button onClick={() => this.props.openEditModal(book.id) } >Edit</Button></p>}
+                                        {profile.id === 0 && <p><Button onClick={() => this.props.openEditModal(book.id) } >Edit</Button></p>}
                                     </Col>
                                 )}
                         </Row>
@@ -223,7 +205,7 @@ class Profile extends React.Component{
                     <Col sm={1}>
                     </Col>
                     <Col>
-                        <h1 className="float-left">{this.state.profileID === "0" ? "Your" : this.state.firstName + "'s"} Favourited Textbooks</h1>
+                        <h1 className="float-left">{profile.id === 0 ? "Your" : profile.firstName + "'s"} Favourited Textbooks</h1>
                     </Col>
                     <Col sm={1}>
                     </Col>
