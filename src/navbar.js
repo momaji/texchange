a423e6d9dc9d480da5a516e6178ea368c31be660
MohammadH98 <mohammad.1011@hotmail.com>
2020-11-16 16:14:28 -0500

    Added button colour change for favourite

--- src/navbar.js
+++ src/navbar.js
@@ -104,7 +104,7 @@ class TexNavbar extends React.Component{
             editProfile={this.props.editProfile.bind(this)}
             addFavourite={this.props.addFavourite.bind(this)}
             removeFavourite={this.props.removeFavourite.bind(this)}
-            userFavourited={this.state.userFavourited}
+            userFavourited={this.props.userFavourited}
           />} />
         </div>
       </Router>
